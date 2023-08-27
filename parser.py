6e52a4365baf9c97389680cdca62b49eeec319e0
ju4ndiegos <123430885+ju4ndiegos@users.noreply.github.com>
2023-08-27 17:17:26 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,8 +1,7 @@
-#parser.py
-
+#Inicializo variables
 x,y=1,1
 
-#0, norte, #1 oriente, #2 occidente, #3 sur
+#0 norte, #1 oriente, #2 occidente (izquierda), #3 sur
 direction=0
 
 def giro():
@@ -13,8 +12,13 @@ def put():
     return True
 
 def move_Forward():
-    if direction==1 and x-1>0:
-        x-=1
-    elif direction==3:
+    if direction==1 and x-1>0: #no se sale de los bordes
         x+=1
-    elif 
+    elif direction ==2:#TODO hay un limite??
+        x-=1
+        
+    elif direction==3: #TODO hay un limite??
+        y+=1
+        
+    elif direction==0 and y-1>0: #no se sale de los bordes
+        y-=1
