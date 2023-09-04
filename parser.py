607ff1d68a79d3130a8e76fdd148726e5f9bdb70
ju4ndiegos <jd.sanchez2>
2023-09-04 10:34:42 -0500

    Avance

--- parser.py
+++ parser.py
@@ -1,24 +1,43 @@
-#Inicializo variables
-x,y=1,1
 
-#0 norte, #1 oriente, #2 occidente (izquierda), #3 sur
-direction=0
+archivo = "prueba.txt"
 
-def giro():
-    direction+=1
-    direction%=4
+text_file=open(archivo)
+
+linea = text_file.readline()
+
+
+variables = {}
+
+def elemento_despues_espacio(string):
+    """Entran strings como por ejemplo "defVar nom 0" o " defVar ..." y me devuelve defVar
+    """
     
-def put():
-    return True
+    elemento="";
+    
+    for i in string:
+        if i!=" ":
+            elemento+=i
+        else:
+            if elemento!="":
+                return elemento
+    
+    
+    
 
-def move_Forward():
-    if direction==1 and x-1>0: #no se sale de los bordes
-        x+=1
-    elif direction ==2:#TODO hay un limite??
-        x-=1
+while linea != "":
+    
+    #sí hay una variable
+    if "defVar" in linea:
         
-    elif direction==3: #TODO hay un limite??
-        y+=1
+        print(elemento_despues_espacio(linea))
+        print(elemento_despues_espacio(linea[len("defVar"):]))
+        ubicacion = linea.find("defVar")
+        indice_nombre_variable = ubicacion+7
         
-    elif direction==0 and y-1>0: #no se sale de los bordes
-        y-=1
+        despues_variable=linea[indice_nombre_variable:]
+        espacio_despues_variable= despues_variable.find(" ")
+        
+        nombre_variable= despues_variable[:espacio_despues_variable]
+        
+    
+    linea = text_file.readline()
