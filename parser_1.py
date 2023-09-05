5de0f4abb6bb446e4366662269e4e2cc34636d47
ju4ndiegos <jd.sanchez2>
2023-09-05 17:07:16 -0500

    parametros

--- parser_1.py
+++ parser_1.py
@@ -7,6 +7,7 @@ linea = text_file.readline()
 
 
 variables = {}
+procedimientos ={}
 
 def elementos_linea(string:str):
     """Entran strings como por ejemplo "defVar nom 0" o " defVar ..." y me devuelve defVar
@@ -17,7 +18,7 @@ def elementos_linea(string:str):
     
     for i in string:
         if i!=" ":
-            elemento+=i
+            elemento+=i        
         if i==" " or i==string[-1]:
             if elemento!="":
                 lista.append(elemento)
@@ -25,6 +26,17 @@ def elementos_linea(string:str):
     
     return lista
     
+def sacar_parametros(sublista:list):
+    parametros=[]
+    if "(" in sublista[0]:
+        for i in sublista:
+            parametros.append(i.replace("(","").replace(")",""))
+            if ")"in i:
+                if i==")":
+                    parametros.pop(-1)
+                break
+    return parametros
+            
     
     
 lista_grande=[]
@@ -34,18 +46,23 @@ while linea != "":
     lista_grande+=lista
     linea = text_file.readline()
 
+simbolos_reservados=["while",";","defVar","defProc","="]
 
 funcionando=True
+Error=False
+
 i=0
-while funcionando:
+while funcionando and Error==False:
     elemento=lista_grande[i]
     
     #sí hay una variable
     if "defVar" == elemento:
         nom_variable = lista_grande[i+1]
         valor= lista_grande[i+2]
-        variables[nom_variable]=valor
-            
+        if nom_variable not in simbolos_reservados  and valor not in simbolos_reservados:
+            variables[nom_variable]=valor
+        else:
+            Error=True
         lista_grande.pop(i)
         lista_grande.pop(i)
         lista_grande.pop(i)
@@ -54,8 +71,9 @@ while funcionando:
         
     if elemento=="defProc":
         nom_proc = lista_grande[i+1]
-        parametros= lista_grande[i+2]#TODO revisar los parametros
-        variables[nom_variable]=valor
+        parametros= sacar_parametros(lista_grande[i+2:])
+        procedimientos[nom_proc]=parametros
+        #print(parametros)
         
     
     #paso para recorrer la lista 
